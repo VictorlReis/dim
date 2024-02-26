818416f0159a949d925801dfbde14fec431ae894
Victor Reis <49236252+VictorlReis@users.noreply.github.com>
2024-02-26 19:06:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,23 +5,23 @@ const openai = new OpenAI({
 })
 
 const runPrompt = async (input) => {
-	const prompt = `Interpret the entered data delimited by triple quotes and transform it into JSON with the following key/value properties: { dsc: string; val: int; }
+	const prompt = `Interpret the entered data delimited by triple quotes and transform it into JSON with the following key/value properties: { "description": string; "value": int; }
 The value should be an integer to represent a currency value, for example: 20.99 should be 2099.
 If there is more than one output, return an array of JSONs.
 Examples:
 Input: supermercado 15.90
-Output: { desc: "supermercado", val: 1590 }
+Output: { "description": "supermercado", "value": 1590 }
 Input: guitar 110.90
-Output: { desc: "guitar", val: 11090 }
+Output: { "description": "guitar", "value": 11090 }
 Input: mcdonalds 59.90  supermercado R$11.30 wjoid 30.99
-Output: [{ desc: "mcdonalds", val: 5990 }, { desc: "supermercado", val: 1130 }, { desc: "wjoid", val: 3099 }]
+Output: [{ "description": "mcdonalds", "value": 5990 }, { "description": "supermercado", "value": 1130 }, { "description": "wjoid", "value": 3099 }]
 Input: headset 240.90 ifood 50 churrasco 39.90
-Output: [{desc: "headset",  val: 24090 }, {desc: "ifood", val: 5000 }, {desc: "churrasco", val: 3990}]
+Output: [{"description": "headset",  "value": 24090 }, {"description": "ifood", val: 5000 }, {"description": "churrasco", "value": 3990}]
 Input: Data;Estabelecimento;Portador;Valor;Parcela
 08/02/2024;FD*food.COM AGENCIA DE;VICTOR REIS;R$ 4,95;-
 18/02/2024;Uno   *Noy   *TRIP;VICTOR REIS;R$ 14,88;-
 29/01/2024;MYPASS;VICTOR REIS;R$ 89,90;-
-Output: [{desc: "IFD*IFOOD.COM", val: 495}, {desc: "ybe *ybe *trip", val: 1488},  {desc: "TOTALPASS", val: 8990}
+Output: [{"description": "IFD*IFOOD.COM", "value": 495}, {"description": "ybe *ybe *trip", "value": 1488},  {"description": "TOTALPASS", "value": 8990}
 The output should not contains spaces or \\n, it should be a valid JSON or array of JSONs
 """${input}"""`;
 
